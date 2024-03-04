adfa9033ee6ce480f29df19389fd799d624be96f
Samarth Mishra <samarth1mishra@gmail.com>
2024-03-04 20:21:30 +0530

    pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Home from './pages/Home';
+import About from './pages/About.jsx';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='text-3xl text-red-500'>App</div>
+    <BrowserRouter>
+    <Routes> 
+    <Route path="/" element={<Home/>}/>
+    <Route path="about" element={<About/>}/>
+    <Route path="/sign-in" element={<SignIn/>}/>
+    <Route path="/sign-up" element={<SignUp/>}/>
+    <Route path="/dashboard" element={<Dashboard/>}/>
+    <Route path="/projects" element={<Projects/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
