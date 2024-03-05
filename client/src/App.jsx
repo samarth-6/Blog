30cda2a6f0c75801f85114e32d63d7ca4b6ca8da
Samarth Mishra <samarth1mishra@gmail.com>
2024-03-06 00:15:06 +0530

    running the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import SignUp from './pages/SignUp';
+import Header from './components/Header.jsx';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes> 
     <Route path="/" element={<Home/>}/>
     <Route path="about" element={<About/>}/>
