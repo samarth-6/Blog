0634114aa904b5757307b9b60fdd8350c8f3f715
Samarth Mishra <samarth1mishra@gmail.com>
2024-03-06 00:35:09 +0530

    connected mongo

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
 const app=express()
+
+mongoose.connect(process.env.MONGO)
+.then(
+    ()=>{console.log('MongoDb is connected')})
+    .catch(
+        (err)=>{ console.log(err)}
+        )
 
 const PORT=3000
 app.listen(PORT,()=>{
