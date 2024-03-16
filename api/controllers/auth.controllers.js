786466cc9760174e0f7f72e1251291dd83902a31
Samarth Mishra <samarth1mishra@gmail.com>
2024-03-16 23:07:55 +0530

    adding middleware

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,25 +1,34 @@
-import User from "../models/user.model.js";
+import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup=async (req,res) =>{
-    const {username,email,password}=req.body;
-   if(!username || !email || !password ||username===''||email===''||password=== ''){
-    return res.status(400).json({message:'All fields are required'})
-   }
-   const hashedPassword= bcryptjs.hashSync(password,10)
-   const newUser=new User({
+
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({
     username,
     email,
-    password:hashedPassword,
-    //password is key hashpassword is value
-    //for username and email their key and value is same that is their username
-   });
-   try {
-    await newUser.save();
-   res.status(201).json({ message: 'User created successfully' });
-   } catch (error) {
-    res.status(500).json({ message: error.message});
-   }
-   
+    password: hashedPassword,
+  });
 
-}
+  try {
+    await newUser.save();
+    res.json('Signup successful');
+  } catch (error) {
+    next(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -3,20 +3,35 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.routes.js';
-dotenv.config()
-const app=express()
-app.use(express.json());
-mongoose.connect(process.env.MONGO)
-.then(
-    ()=>{console.log('MongoDb is connected')})
-    .catch(
-        (err)=>{ console.log(err)}
-        )
+import { errorHandler } from './utils/error.js'; // Import the errorHandler function
+dotenv.config();
 
-const PORT=3000
-app.listen(PORT,()=>{
-    console.log(`Server running on PORT  ${PORT}`)
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO)
+    .then(() => { console.log('MongoDb is connected') })
+    .catch((err) => { console.log(err) });
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
 });
+
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
+
+export default app;
+
 
