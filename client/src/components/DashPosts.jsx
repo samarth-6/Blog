3073fb86530d20e93096ab034b158f1ee8c05e2d
Samarth Mishra <samarth1mishra@gmail.com>
2024-05-15 20:55:47 +0530

    set show more result of post

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -8,7 +8,25 @@ import { set } from 'mongoose';
 const DashPosts = () => {
   const {currentUser}=useSelector((state)=>state.user)
   const [userPosts,setUserPosts]=useState([])
+  const [showMore,setShowMore]=useState(true);
   console.log(userPosts);
+  const handleShowMore=async()=>{
+    const startIndex=userPosts.length;
+    try{
+      const res=await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`)
+      const data=await res.json();
+      if(res.ok){
+        setUserPosts((prev)=>[...prev,...data.posts]);
+        if(data.posts.length<9){
+          setShowMore(false);
+        }
+      }
+
+    }catch(error){
+      console.log(error.message)
+    }
+
+  }
   useEffect(()=>{
      const fetchPosts = async()=>{
        try {
@@ -16,6 +34,9 @@ const DashPosts = () => {
         const data=await res.json()
         if(res.ok){
           setUserPosts(data.posts)
+          if(data.posts.length<9){
+            setShowMore(false);
+          }
         }
         
        } catch (error) {
@@ -90,6 +111,13 @@ const DashPosts = () => {
             ))}
           
           </Table>
+          {
+            showMore && (
+              <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+                Show more
+              </button>
+            )
+          }
           
           
       </>
