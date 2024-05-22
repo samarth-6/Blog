9c604f77ceabc92129cdc26a0f4c1c03c0d283b1
Samarth Mishra <samarth1mishra@gmail.com>
2024-05-23 00:48:53 +0530

    show comments of a post

--- api/controllers/user.contollers.js
+++ api/controllers/user.contollers.js
@@ -112,3 +112,16 @@ export const getUsers=async (req,res,next)=>{
     next(error);
   }
 }
+
+export const getUser=async(req,res,next)=>{
+  try {
+    const user=await User.findById(req.params.userId)
+    if(!user){
+      return next(errorHandler(404,'USER NOT FOUND'))
+    }
+    const {password,...rest}=user._doc;
+    res.status(200).json(rest)
+  } catch (error) {
+    next(error)
+  }
+}
