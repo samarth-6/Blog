4e5a470065450b90a5cade09fb4ddcf55563a932
Samarth Mishra <samarth1mishra@gmail.com>
2024-05-26 02:03:13 +0530

    Add like functionality to comments

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -3,13 +3,14 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
-import Comment from './comment.jsx';
+import Comment from './Comment.jsx';
 
  function CommentSection({postId}) {
     const {currentUser}=useSelector(state=>state.user)
     const [comment,setComment]=useState('');
     const [commentError,setCommentError]=useState(null);
     const [comments,setComments]=useState([]);
+    const navigate=useNavigate();
     console.log(comments)
     const handleSubmit=async(e)=>{
       e.preventDefault()
@@ -49,6 +50,34 @@ import Comment from './comment.jsx';
       };
       getComments();
     }, [postId]);
+
+    const handleLike = async (commentId) => {
+      try {
+        if (!currentUser) {
+          navigate('/sign-in');
+          return;
+        }
+        const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+          method: 'PUT',
+        });
+        if (res.ok) {
+          const data = await res.json();
+          setComments(
+            comments.map((comment) =>
+              comment._id === commentId
+                ? {
+                    ...comment,
+                    likes: data.likes,
+                    numberOfLikes: data.likes.length,
+                  }
+                : comment
+            )
+          );
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
         {currentUser?
@@ -111,6 +140,7 @@ import Comment from './comment.jsx';
             <Comment
               key={comment._id}
               comment={comment}
+              onLike={handleLike}
             />
           ))}
         </>
